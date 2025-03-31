0e29d9adf1bbb6fef48077d8f614a875fe11bb59
Ignatkick <ignatkikin@yandex.ru>
2025-04-01 02:23:43 +0800

    исправление ошибки

--- stats/stat-dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ stats/stat-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -2,6 +2,7 @@ package ru.practicum;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,7 +21,7 @@ public class EndpointHitDto {
     private String uri;
     @NotBlank
     private String ip;
-    @NotBlank
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
