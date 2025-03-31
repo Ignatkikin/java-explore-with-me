5ca7f09647d5c00ee64682ef1e851e7ad3ed11b2
Ignatkick <ignatkikin@yandex.ru>
2025-03-31 23:36:38 +0800

    Первая проверка

---
+++ stats/stat-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -0,0 +1,31 @@
+package ru.practicum.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "hits")
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EndpointHit {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "app", nullable = false)
+    private String app;
+    @Column(name = "uri", nullable = false)
+    private String uri;
+    @Column(name = "ip", nullable = false)
+    private String ip;
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+}
